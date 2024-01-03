80c86a72a0bbbd9801af3f7886dfdf7c465741c4
Valerii Butorin <butorin.v.a@niuitmo.ru>
2024-01-03 21:57:18 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -9,7 +9,7 @@ do
   git diff --name-only HEAD^ HEAD | grep $service > /dev/null
   changed=$?
   if [ $exist -ne 0 ]; then
-    echo Container $service not found, build started...
+    echo Container $service not found, build started
     docker-compose build $service
   elif [ $changed -eq 0 ]; then
     echo Service $service has changed, build started...
