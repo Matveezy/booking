3943fd5d11b7993f3a9e3f88b9133c049a3dcbde
Valerii Butorin <butorin.v.a@niuitmo.ru>
2024-01-06 16:42:36 +0000

    Butorin/swagger

--- files/src/main/java/booking/files/controller/ReceiptController.java
+++ files/src/main/java/booking/files/controller/ReceiptController.java
@@ -3,15 +3,9 @@ package booking.files.controller;
 import booking.files.document.Receipt;
 import booking.files.service.ReceiptService;
 import lombok.RequiredArgsConstructor;
-import lombok.SneakyThrows;
 import org.springframework.core.io.ByteArrayResource;
-import org.springframework.core.io.FileSystemResource;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
-
-import java.io.File;
-import java.util.Base64;
 
 @RequiredArgsConstructor
 @RequestMapping("/receipt")
