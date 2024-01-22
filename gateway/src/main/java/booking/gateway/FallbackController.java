f4a8e2a79ee9112a85d30080032f70554276658a
Anvar Gazizov <312444@niuitmo.ru>
2024-01-22 19:33:56 +0000

    Circuit breaker

--- gateway/src/main/java/booking/gateway/FallbackController.java
+++ gateway/src/main/java/booking/gateway/FallbackController.java
@@ -2,13 +2,15 @@ package booking.gateway;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/fallback")
 public class FallbackController {
-    @RequestMapping("/orderFallBack")
+    @GetMapping("/message")
     public ResponseEntity<String> fallback() {
-        return new ResponseEntity<>("It is taking too long to respond. Please try again later.", HttpStatus.INTERNAL_SERVER_ERROR);
+        return new ResponseEntity<>("Service is taking too long to respond or is down. Please try again later", HttpStatus.GATEWAY_TIMEOUT);
     }
 }
